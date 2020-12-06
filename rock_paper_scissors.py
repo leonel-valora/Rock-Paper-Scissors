20af952e7aea22c1d5dfa59713df767295f18433
leonel <leonel01sys@gmail.com>
2020-12-06 15:41:48 -0600

    stage 4 - rating with files

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,19 +1,50 @@
 import random
 # Write your code here
-valid_inputs = ["rock", "paper", "scissors"]
-hand_shape = input()
-while hand_shape != "!exit":
-    if hand_shape in valid_inputs:
-        msg = ""
-        computer_shape = random.choice(valid_inputs)
-        if (hand_shape == "rock" and computer_shape == "scissors") or (hand_shape == "paper" and computer_shape == "rock") or (hand_shape == "scissors" and computer_shape == "paper"):
-            msg = "Well done. The computer chose "+ computer_shape +" and failed"
-        elif hand_shape == computer_shape:
-            msg = "There is a draw ("+ computer_shape +")" 
+game_move = ["rock", "paper", "scissors"]
+rating_file = open("rating.txt", 'r')
+# start the game
+user_name = input("Enter your name: ")
+print("Hello,", user_name)
+
+def initUserScore(user, file):
+    score = 0
+    for line in file:
+        if user in line:
+            user, score = line.split()
+            score = int(score)
+            break;
+    return score
+
+user_score = initUserScore(user_name, rating_file)
+
+def userInput():
+    option = input()
+    while option not in game_move and option != "!exit":
+        if option == "!rating":
+            print("Your rating:", user_score)
         else:
-            msg = "Sorry, but the computer chose "+computer_shape
-        print(msg)
-    else:
-        print("Invalid input")
-    hand_shape = input()
+            print("Invalid input")
+        option = input()
+    return option
+     
+def checkWinner(user_move, computer_move):
+    global user_score
+    msg = "Sorry, but the computer chose "+computer_move
+    if (user_move == "rock" and computer_move == "scissors") or (user_move == "paper" and computer_move == "rock") or (user_move == "scissors" and computer_move == "paper"):
+        msg = "Well done. The computer chose "+ computer_move +" and failed"
+        user_score += 100
+    elif user_move == computer_move:
+        msg = "There is a draw ("+ computer_move +")"
+        user_score += 50
+    print(user_score)
+    return msg
+    
+user_move = userInput()
+while user_move != "!exit":
+    computer_move = random.choice(game_move)
+    msg = checkWinner(user_move, computer_move)
+    print(msg)
+    user_move = userInput()
+
 print("Bye!")
+    
