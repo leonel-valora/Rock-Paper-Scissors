387e270cce9f65315aeb1212fd68008bfc1322b7
leonel <leonel01sys@gmail.com>
2020-12-07 13:07:34 -0600

    stage 5 - adding more moves

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,10 +1,30 @@
 import random
 # Write your code here
-game_move = ["rock", "paper", "scissors"]
+winning_cases = {
+     'water' : ['scissors', 'fire', 'rock', 'hun', 'lightning', 'devil', 'dragon'],
+    'dragon' : ['snake', 'scissors', 'fire', 'rock', 'gun', 'lightning', 'devil'],
+    'devil' : ['tree', 'human', 'snake', 'scissors', 'fire', 'rock', 'gun'],
+    'gun' : ['wolf', 'tree', 'human', 'snake', 'scissors', 'fire', 'rock'],
+    'rock' : ['sponge', 'wolf', 'tree', 'human', 'snake', 'scissors', 'fire'],
+    'fire' : ['paper', 'sponge', 'wolf', 'tree', 'human', 'snake', 'scissors'],
+    'scissors' : ['air', 'paper', 'sponge', 'wolf', 'tree', 'human', 'snake'],
+    'snake' : ['water', 'air', 'paper', 'sponge', 'wolf', 'tree', 'human'],
+    'human' : ['dragon', 'water', 'air', 'paper', 'sponge', 'wolf', 'tree'],
+    'tree' : ['devil', 'dragon', 'water', 'air', 'paper', 'sponge', 'wolf'],
+    'wolf' : ['lightning', 'devil', 'dragon', 'water', 'air', 'paper', 'sponge'],
+    'sponge' : ['gun', 'lightning', 'devil', 'dragon', 'water', 'air', 'paper'],
+    'paper' : ['rock', 'gun', 'lightning', 'devil', 'dragon', 'water', 'air'],
+    'air' : ['fire', 'rock', 'gun', 'lightning', 'devil', 'dragon', 'water'],
+    'lightning' : ['tree', 'human', 'snake', 'scissors', 'fire', 'rock', 'gun']
+}
+default_moves = ["rock", "paper", "scissors"]
 rating_file = open("rating.txt", 'r')
 # start the game
 user_name = input("Enter your name: ")
 print("Hello,", user_name)
+declared_user_moves = input()
+print("Okay, let's start")
+game_move = default_moves if declared_user_moves == "" else declared_user_moves.split(",")
 
 def initUserScore(user, file):
     score = 0
@@ -30,13 +50,12 @@ def userInput():
 def checkWinner(user_move, computer_move):
     global user_score
     msg = "Sorry, but the computer chose "+computer_move
-    if (user_move == "rock" and computer_move == "scissors") or (user_move == "paper" and computer_move == "rock") or (user_move == "scissors" and computer_move == "paper"):
-        msg = "Well done. The computer chose "+ computer_move +" and failed"
-        user_score += 100
-    elif user_move == computer_move:
+    if user_move == computer_move:
         msg = "There is a draw ("+ computer_move +")"
         user_score += 50
-    print(user_score)
+    elif computer_move in winning_cases[user_move]:
+        msg = "Well done. The computer chose "+ computer_move +" and failed"
+        user_score += 100
     return msg
     
 user_move = userInput()
@@ -47,4 +66,3 @@ while user_move != "!exit":
     user_move = userInput()
 
 print("Bye!")
-    
